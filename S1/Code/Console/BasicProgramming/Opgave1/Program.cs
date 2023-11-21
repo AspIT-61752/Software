94d25c00f9c15152e1f3dd7e97cc0a89bf631bf5
Tobsi <61752@edu.campusvejle.dk>
2023-11-21 17:09:49 -0600

    Done for today 21 Nov 2023
    Forgot to push to GitHub.

--- S1/Code/Console/BasicProgramming/Opgave1/Program.cs
+++ S1/Code/Console/BasicProgramming/Opgave1/Program.cs
@@ -1,7 +1,47 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
 
 namespace Opgave1
 {
+    public enum BMIResult
+    {
+        [Description("Undervægtig Mand")]
+        underWeightM = 1,
+
+        [Description("Undervægtig Kvinde")]
+        underWeightF = 2,
+
+        [Description("Normalvægtig Mand")]
+        normalWeightM = 3,
+
+        [Description("Normalvægtig Kvinde")]
+        normalWeightF = 4,
+
+        [Description("Overvægtig Mand")]
+        overWeightM = 5,
+
+        [Description("Overvægtig Kvinde")]
+        overWeightF = 6,
+
+        [Description("Svært overvægtig Mand")]
+        sOverWeightM = 7,
+
+        [Description("Svært overvægtig Kvinde")]
+        sOverWeightF = 8,
+    }
+
+    public enum CLIChoices
+    {
+        exit = 0,
+        start = 1,
+        about = 2,
+        randomNumber = 3,
+
+    }
+
     internal class Program
     {
         // Public variables
@@ -9,14 +49,25 @@ namespace Opgave1
 
         static void Main(string[] args)
         {
+            // This is for consistency.
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+
             //WriteToConsole(GetUserInput());
             //Hello();
             //CountUp();
             //TaxiCalculator();
 
             //ConvertReadLine();
-            NameAndHobby();
+            //NameAndHobby();
+            //BMICalc();
+            Console.Write("gaming, ");
+            CLIColoredText("more ");
+            Console.Write("and something else");
+            CLIMenu();
 
+            // Wait for input
+            Console.ReadKey();
         }
 
         // 2.1
@@ -141,30 +192,189 @@ namespace Opgave1
             // Private variables
             int currentYear = int.Parse(DateTime.Now.Year.ToString()); // Get current year
 
-            //string name = "John";
-
+            // Prompt 1
             Console.Write("God dag. Kan du skrive dit navn: ");
             name = Console.ReadLine();
 
+            // Prompt 2
             Console.Write($"Hej {name}, hvad er din hobby?: ");
             hobby = Console.ReadLine();
 
+            // Prompt 3
             Console.Write($"Interesandt. Hvor mange år har du haft {hobby} som hobby?: ");
-            //hobbyYears = ((int)(Console.ReadLine()));
-            //int.TryParse(str, out hobbyYears);
-            //hobbyYears = Convert.ToInt32(Convert.ToDouble(Console.ReadLine()));
 
             //Int doesn't recognize floatingpoint numbers. So you have to convert it a lot of times. 
             hobbyYears = Convert.ToInt32(Math.Floor(double.Parse(Console.ReadLine())));
 
+            // Prompt 4
             Console.Write($"Hvor gammel er du?: ");
             age = Convert.ToInt32(Math.Floor(double.Parse(Console.ReadLine())));
 
+            // Math
             startYear = currentYear - hobbyYears;
             userHobbyStartAge = age - hobbyYears;
 
-
+            // Displaying the info
             Console.WriteLine($"Hej {name}, din hobby er {hobby}. Du begyndte at dyrke {hobby} i {startYear} i en alder af {userHobbyStartAge} år.");
+        }
+
+        // 5.7
+        static void BMICalc()
+        {
+            /*
+                Indtast din højde i meter: 1,78
+                Indtast din vægt i kilo: 74
+                Indtast køn M/K: M
+                BMI: 23,355636914530993561419012751
+                Normalvægtig mand
+
+                https://nexs.ku.dk/forskning/vidensbanken/bmi-udregning/ 
+                https://hjemmetraeningudstyr.dk/bmi-beregner/
+                Formel: weight/height^2
+                Under 18,5	Undervægtig
+                18,5-25	Normalvægtig
+                25-30	Overvægtig
+                Over 30	Svært overvægtig
+
+                BUGS:
+                    Can't use . because C# looks at the local standardized decimal separator character (In Denmark it's ,).
+                    I haven't made any try-catch error handling. 
+            */
+
+            double height = 0.2;
+            double weight = 36.4;
+            string gender = "N";
+
+            double userBMI = 15.3;
+
+
+            Console.Write("Indtast din højde i meter: ");
+            double.TryParse(Console.ReadLine(), out height);
+
+            Console.Write("Indtast din vægt i kilo: ");
+            double.TryParse(Console.ReadLine(), out weight);
+
+            Console.Write("Indtast dit køn M/K: ");
+            gender = Console.ReadLine().ToLower();
+
+            Console.WriteLine($"GENDER: {gender}");
+
+            userBMI = weight / Math.Pow(height, 2);
+
+            Console.WriteLine($"BMI: {userBMI}");
+
+            /*
+             CPR number strat:
+            Uneven numbers => Male
+            To check if it's a male or female person divide by 2 and check if there's a remainder. 
+            Starts at underwheight
+             */
+
+            // Checks if they're male
+            if (gender.StartsWith("m"))
+            {
+                // Under
+                if (userBMI <= 18.5)
+                {
+                    Console.Write(GetEnumDescription(BMIResult.underWeightM));
+                }
+                // Normal
+                else if (userBMI > 18.5 && userBMI <= 25)
+                {
+                    Console.Write(GetEnumDescription(BMIResult.normalWeightM));
+                }
+                // Over
+                else if (userBMI > 25 && userBMI <= 30)
+                {
+                    Console.Write(GetEnumDescription(BMIResult.overWeightM));
+                }
+                // Very
+                else
+                {
+                    Console.Write(GetEnumDescription(BMIResult.sOverWeightM));
+                }
+            }
+            else
+            {
+                // Female
+                // Under
+                if (userBMI <= 18.5)
+                {
+                    Console.Write(GetEnumDescription(BMIResult.underWeightF));
+                }
+                // Normal
+                else if (userBMI > 18.5 && userBMI <= 25)
+                {
+                    Console.Write(GetEnumDescription(BMIResult.normalWeightF));
+                }
+                // Over
+                else if (userBMI > 25 && userBMI <= 30)
+                {
+                    Console.Write(GetEnumDescription(BMIResult.overWeightF));
+                }
+                // Very
+                else
+                {
+                    Console.Write(GetEnumDescription(BMIResult.sOverWeightF));
+                }
+            }
+        }
+
+        // 6.5 START
+
+        // TODO: Rewrite all of this. I read the assignment wrong. 
+        static void CLIMenu()
+        {
+            CLIPrintMenu();
+
+            Console.Write("User choice: ");
+            // Choice
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string coice = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+
+        static void CLIPrintMenu()
+        {
+            Console.Clear(); // Clears the console
+
+            Console.WriteLine("=== Menu ===");
+
+            /*
+             === Menu ===
+            empty
+                1)  Option 1
+                2)  Option 2
+                0)  Exit
+             */
+
+            CLIColoredText(" 0");
+            Console.WriteLine(") Exit");
+        }
+
+        static void CLIColoredText(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(msg);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        // 6.5 END
+
+        // From https://codereview.stackexchange.com/questions/157871/method-that-returns-description-attribute-of-enum-value
+        public static string GetEnumDescription(Enum value)
+        {
+            // How it works
+            // https://stackoverflow.com/questions/28352072/what-does-question-mark-and-dot-operator-mean-in-c-sharp-6-0
+            // https://learn.microsoft.com/en-in/dotnet/csharp/language-reference/operators/null-coalescing-operator // The null-coalescing operator ?? returns the value of its left-hand operand if it isn't null;
+            return value.GetType().GetMember(value.ToString()).FirstOrDefault()?.GetCustomAttribute<DescriptionAttribute>()?.Description ?? value.ToString();
+        }
+
+        // A quick debugging method I use
+        public static void dbgStr(string variableName, string value)
+        {
+            Console.WriteLine($"{variableName}: {value}");
         }
 
     }
